9695fe8e8d9580e7cab51aa9cd467e157ea465a7
Yahya Alazzi <yahyaalazzi@Yahyas-MacBook-Air.local>
2025-06-27 16:09:37 +0300

    dddss

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,11 @@
-// next-sitemap.config.js
 module.exports = {
   siteUrl: 'https://3zzo.com',
   generateRobotsTxt: true,
+  exclude: ['/privacy-policy', '/login'],      // أمثلة على صفحات تستثنيها
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://3zzo.com/sitemap-0.xml',
+      'https://3zzo.com/extra-sitemap.xml',
+    ]
+  }
 }
